ae0111c56800a04b7102e898f882b9f121bfd766
mkaletaa <kaleta222@gmail.com>
2022-12-10 17:11:22 +0100

    dashborad

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -8,43 +8,14 @@ import {getFirestore, collection, setDoc, getDocs, query, where, updateDoc, doc}
 import { useInfiniteQuery } from 'react-query';
 
 export default function Dashboard({users}:any) {
-const [par, setPar] = useState(0)
-  const fetchPosts=({pageParam=0})=>{
-    // alert(pageParam)
-    setPar(prev=>prev+1)
-    console.log('pageparam', pageParam)
-    return axios.get(`${process.env.NEXT_PUBLIC_URL}/api/users/ZQ2YlBxJ03Wua1gkQCSPujTedVO2/posts/${par}`)
-  }
 
-  const {data, isLoading, hasNextPage, fetchNextPage} = useInfiniteQuery(
-    ['colors'],
-      fetchPosts,{
-      refetchInterval: 1000,
-      getNextPageParam: (_lastPage, pages)=>{
-       return pages.length+1
-      },
-      // onSuccess: ()=>{console.log('data', data?.pages[1].data[0].text)}
-    }
-  )
 
 // useEffect(()=>{
 // //   alert(data?.pages)
 // console.log(par)
 // }, [par])
   return (
-    <>
-      <div className="topBarMain">main</div>
-      <div className="topBarRight">right</div>
-
-      <div className="main">
-        <button  onClick={fetchNextPage}>setDoc</button>
-        {par}
-          {
-            data?.pages[0].data.map((a:any)=>{return (<div key={a.id}>{a.text}</div>)})
-          }
-      </div>
-
-    </>
+<></>
   )
 }
 
